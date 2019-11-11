624313268a1bb1a201f4a2396fadfbf9d1fc5991
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-11 03:19:57 +0100

    Projectile Explosion effect added, camera configs updated

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float projectileLifeTime;
+    public float projectileSpeed;
+    public GameObject explosion;
+
+    private void Start()
+    {
+        // Call function after a delay
+        Invoke("DestroyProjectile", projectileLifeTime);
+    }
+
+    private void Update()
+    {
+        MoveProjectile();
+    }
+
+    private void MoveProjectile()
+    {
+        // Move projectile frame rate independently 
+        transform.Translate(Vector2.up * projectileSpeed * Time.deltaTime);
+    }
+
+    private void DestroyProjectile()
+    {
+        // Spawn explosion effect
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        // Destroy projectile
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject projectile;
+    public Transform shootingPoint;
+    public float periodBetweenShots;
+    private float shootingTime;
+
     private void Update()
     {
         WeaponDirectionFollowingMouse();
+        Shooting();
     }
 
     // Adjust weapon direction to mouse
@@ -16,9 +22,25 @@ public class Weapon : MonoBehaviour
         Vector2 weaponDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         // The angle the weapon must rotate around to face the cursor
         float angle = Mathf.Atan2(weaponDirection.y, weaponDirection.x) * Mathf.Rad2Deg;
-        // Convert angle to Unity rotation
+        // Convert angle to Unity rotation 
         Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
         // Apply the rotation to weapon 
         transform.rotation = rotation;
+    }
+
+    // Handle shooting
+    private void Shooting()
+    {
+        // Check if Left mouse button is pressed
+        if (Input.GetMouseButton(0))
+        {
+            // Check if current time in game is higher than shooting time(Can we shoot?)
+            if (Time.time >= shootingTime)
+            {
+                // Create a projectile
+                Instantiate(projectile, shootingPoint.position, transform.rotation);
+                shootingTime = Time.time + periodBetweenShots;
+            }
+        }
     }
 }
