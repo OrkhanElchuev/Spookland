f40e76814e7e7819b83750938778efd51157078d
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-13 18:16:53 +0100

    Flying enemy shooting added

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class EnemyProjectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Public
+    public float projectileSpeed;
+    public int damage;
+
+    // Private
+    private Player playerScript;
+    private Vector2 targetPosition;
+
+    private void Start()
     {
-        
+        // Initial assignments
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        targetPosition = playerScript.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MoveProjectile();
+    }
+
+    // Handle the movement of projectile
+    private void MoveProjectile()
+    {
+        // Check the distance between projectile and player
+        if (Vector2.Distance(transform.position, targetPosition) > 0.2f)
+        {
+            // If distance between projectile and player is (> 0.2) continue moving towards player
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition,
+             projectileSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Otherwise destroy projectile 
+            Destroy(gameObject);
+        }
+    }
+
+    // Handle projectile collision
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if projectile collided with player
+        if (collision.tag == "Player")
+        {
+            playerScript.TakeDamage(damage);
+            // Destroy projectile
+            Destroy(gameObject);
+        }
     }
 }
