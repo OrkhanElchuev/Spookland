e9c6a3487026b9bf4e1269002e2c7f06195c82da
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-17 21:25:54 +0100

    Boss enemy is done

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -8,6 +8,8 @@ public class Boss : MonoBehaviour
     public int bossHealth;
     public Enemy[] enemies;
     public float spawnOffset;
+    public int bossDamage;
+
 
     // Private
     private int halfHealth;
@@ -19,21 +21,32 @@ public class Boss : MonoBehaviour
         bossAnimator = GetComponent<Animator>();
     }
 
-    // Handle damage and destroy enemy
-    public void TakeDamage(int damageAmount)
+    // Handle collision of boss with player
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        // In case of colliding with player deal damage
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Player>().TakeDamage(bossDamage);
+        }
+    }
+
+    // Handle damage
+    public void TakeDamage(int damageAmount)
+    { 
         bossHealth -= damageAmount;
         if (bossHealth <= 0)
         {
             Destroy(this.gameObject);
         }
-
+        // In case of reaching half health points trigger chasing behaviour
         if (bossHealth <= halfHealth)
         {
             bossAnimator.SetTrigger("FromWalkToRun");
         }
-
+        // Choose random enemy
         Enemy randomEnemy = enemies[Random.Range(0, enemies.Length)];
+        // Instantiate enemy away from boss using offset
         Instantiate(randomEnemy, transform.position + new Vector3(spawnOffset, spawnOffset, 0), transform.rotation);
     }
 }
