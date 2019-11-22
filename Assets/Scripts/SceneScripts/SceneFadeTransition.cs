d6649f73e9053150c2d24710136cf69b5b739f6c
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-22 20:25:22 +0100

    Buttons functionality added, script folders arranged, scene transitions created

---
+++ Assets/Scripts/SceneScripts/SceneFadeTransition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFadeTransition : MonoBehaviour
+{
+    // Private
+    private Animator transitionAnimation;
+
+    private void Start()
+    {
+        transitionAnimation = GetComponent<Animator>();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        StartCoroutine(Transition(sceneName));
+    }
+
+    IEnumerator Transition(string sceneName)
+    {
+        transitionAnimation.SetTrigger("End");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Quit the application 
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
