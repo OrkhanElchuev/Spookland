3ae2289bdf14b68797d5b1d945d314be589e102a
Orkhan-Elchuev <orkhan.elchuev@gmail.com>
2019-11-22 21:39:46 +0100

    Background music added to game

---
+++ Assets/Scripts/SceneScripts/Music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    private static Music musicInstance;
+
+    private void Awake()
+    {
+        // Check if music instance exists
+        if (musicInstance == null)
+        {
+            musicInstance = this;
+            // Dont destroy on Load of new scene
+            DontDestroyOnLoad(musicInstance);
+        }
+        else
+        {
+            Destroy(musicInstance);
+        }
+    }
+}
